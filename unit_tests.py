c2a5fde31061abcf21452b12919d530cc1b2443a
Paulo Ledo <paulofledo@gmail.com>
2015-09-03 10:44:41 -0300

    Unit tests done

---
+++ unit_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from talk_flaskrs import flaskrs
+#import talk_flaskrs
+import pytest
+
+def test_db_connection():
+    """docstring for test_db_connection"""
+    assert flaskrs.connect_db()
